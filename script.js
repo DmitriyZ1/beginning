b537a3e2b4418cd7af55a9422685314472172326
Dmitriy <zavoronk@mail.ru>
2021-08-05 00:52:55 +0300

    nuw

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ arr.forEach((el)=>{
     let div = document.createElement('div');
     div.classList.add('cart');
     let p = document.createElement('p');
-    p.textContent = el;
+    
+    let text = el.split('');
+    //text.splice(0,1,text[0].toUpperCase());
+    text[0] = text[0].toUpperCase();
+    text = text.join('');
+   
+    console.log(text)
+    
+    
+    p.textContent = text;
     
     div.append(p);
     document.querySelector('body').append(div);
